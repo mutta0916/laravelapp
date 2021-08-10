363c3b14f12a677655f5e5412300199a3075f914
mutta0916 <mutta0916@gmail.com>
2021-08-10 19:26:56 +0900

    一時コミット

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function () {
-    return '<html><body><h1>Hello</h1><p>これはサンプルです！</p></body></html>';
-});
+Route::get('hello', [HelloController::class, 'index']);
+// Route::get('hello/other', [HelloController::class, 'other']);

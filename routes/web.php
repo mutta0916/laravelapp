fc9550f30df79a245a5e2295b574c7ca88f4e140
mutta0916 <mutta0916@gmail.com>
2021-08-21 19:35:08 +0900

    Chapter5 データベースの利用

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\PersonController;
 use App\Http\Controllers;
 use App\Http\Middleware\HelloMidleware;
 
@@ -36,3 +37,9 @@ Route::get('hello/del', [HelloController::class, 'del']);
 Route::post('hello/del', [HelloController::class, 'remove']);
 
 Route::get('hello/show', [HelloController::class, 'show']);
+
+Route::get('person', [PersonController::class, 'index']);
+Route::get('person/find', [PersonController::class, 'find']);
+Route::post('person/find', [PersonController::class, 'search']);
+Route::get('person/add', [PersonController::class, 'add']);
+Route::post('person/add', [PersonController::class, 'create']);
